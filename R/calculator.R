242dc943578ca3eaf9e4a9ef2882f7dd0e85f590
brunykrijgsman <bruny_krijgsman@hotmail.com>
2024-05-22 17:09:24 +0200

    Feedback
    Adjustments made to the documentation of different functions based on received feedback

--- R/calculator.R
+++ R/calculator.R
@@ -1,24 +1,35 @@
-observeCalculator <- function(input, output) {
+# Calculator function that appears when the action button 'Calculator' is clicked.
+# It takes 2 numbers and a operator and outputs the result.
+
+calculator <- function(input, output) {
+  
   # Open calculator modal
   shiny::observeEvent(input$calculator_button, {
     shiny::showModal(
       shiny::modalDialog(
         title = "Calculator",
-        shiny::div(
-          style = "display: flex; flex-direction: column;",
-          shiny::numericInput("num1_calc", "Number 1:", value = NULL),
-          shiny::selectInput(
-            "operator_calc",
-            "Operator:",
-            choices = c("+", "-", "*", "/")
-          ),
-          shiny::numericInput("num2_calc", "Number 2:", value = NULL),
-          shiny::textOutput("calc_result")
-        ),
+          shiny::fluidRow(
+            # Create two columns to display the calculator input and the image next to each other
+            shiny::column(6,
+              shiny::div(
+                style = "display: flex; flex-direction: column; align-items: center;",
+                shiny::numericInput("num1_calc", "Number 1:", value = NULL),
+                shiny::selectInput("operator_calc", "Operator:",
+                             choices = c("+", "-", "*", "/")),
+                shiny::numericInput("num2_calc", "Number 2:", value = NULL),
+                shiny::textOutput("calc_result"))),
+            
+            # Add image
+            shiny::column(6,
+              shiny::img(src = "/static/calculator.png", height = "200px", width = "160px"))
+            ),
+        
+        # Action buttons in the footer
         footer = shiny::tagList(
-          shiny::actionButton("calculate", "Calculate"),
-          shiny::actionButton("close_calculator", "Close")
+              shiny::actionButton("calculate", "Calculate"),
+              shiny::actionButton("close_calculator", "Close")
         ),
+        
         easyClose = TRUE
       )
     )
@@ -27,13 +38,13 @@ observeCalculator <- function(input, output) {
   # Perform calculation and display result
   shiny::observeEvent(input$calculate, {
     shiny::req(input$num1_calc, input$num2_calc, input$operator_calc)
-    result <- switch(
-      input$operator_calc,
-      "+" = input$num1_calc + input$num2_calc,
-      "-" = input$num1_calc - input$num2_calc,
-      "*" = input$num1_calc * input$num2_calc,
-      "/" = input$num1_calc / input$num2_calc
-    )
+    result <- switch(input$operator_calc,
+                     "+" = input$num1_calc + input$num2_calc,
+                     "-" = input$num1_calc - input$num2_calc,
+                     "*" = input$num1_calc * input$num2_calc,
+                     "/" = input$num1_calc / input$num2_calc)
+    
+    # Output result
     output$calc_result <- shiny::renderText({
       paste("Result:", result)
     })
